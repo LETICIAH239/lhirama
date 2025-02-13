d51111b26ef7c3948cca04cc334cb5c7a4cadeb1
Leticia Hirama <leticia.hirama@gmail.com>
2025-02-13 18:26:39 +0100

    function Square

---
+++ funcao2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int AreaQuadrado (int , int);
+int s;
+
+int main(void){
+
+int area;
+ 
+AreaQuadrado(5,5);
+printf(" Resultado:%i \n", s);
+
+return 0;
+}
+
+int AreaQuadrado(int lado, int altura){
+
+     
+     s=lado*altura;
+
+     return s;
+}
